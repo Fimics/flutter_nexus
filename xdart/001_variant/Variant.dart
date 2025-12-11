b240d1a328f950a87abd13b3b593f0e583dd5ef8
noetix <lpjhblpj@163.com>
2025-12-11 17:33:30 +0800

    变量 final const is as Object _通配符变量 1

--- xdart/001_variant/Variant.dart
+++ xdart/001_variant/Variant.dart
@@ -12,8 +12,8 @@ void main(){
   print(description);
   print(name);
 
-  const Object i =3;
-  const list =[i as int];
-  const map ={if(i is int) i:'int'};
-  const set = {if (list is List<int>) ...list}
+  const Object i = 3; // Where i is a const Object with an int value...
+  const list = [i as int]; // Use a typecast.
+  const map = {if (i is int) i: 'int'}; // Use is and collection if.
+  const set = {if (list is List<int>) ...list}; // ...and a spread.
 }
